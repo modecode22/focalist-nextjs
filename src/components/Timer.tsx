873c11db66f3c403b8e9c79110ac83254cc51cf1
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-03-16 05:55:18 +0100

    alot

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -35,51 +35,94 @@ const Timer = () => {
 
   const minutes = Math.floor(seconds / 60);
   const formattedSeconds = `${seconds % 60}`.padStart(2, "0");
-
-
+const progressWidth = 100-(seconds * 100) / session;
+console.log(progressWidth);
 
   return (
-    <section className="fixed bottom-0 right-1/2 left-1/2 -translate-x-[50%] rounded-t-full  bg-darkform p-2 gap-2 flex flex-col justify-center items-center w-32">
-      <div className="w-16">
-        <CircularProgressbar
-          className={`${
-            isRunning
-              ? "fill-white stroke-main100/80"
-              : "fill-white/50 stroke-main100/40"
-          }`}
-          value={(seconds * 100) / session}
-          text={`${minutes}:${formattedSeconds}`}
-        />
-      </div>
+    <>
+      <section className="duration-100 transition-all group absolute overflow-hidden w-[128px] h-12 bg-black/80  bottom-2 right-1/2 left-1/2 -translate-x-[50%] rounded-xl backdrop-blur-md shadow-black shadow-sm">
+        <div
+          style={{ width: `${progressWidth}%` }}
+          className="duration-100 transition-all absolute bg-gradient-to-br from-main100 to-main200  h-full rounded-sm"
+        ></div>
+        <div className="duration-100 transition-all group-hover:hidden z-30 absolute w-full  h-full flex justify-center items-center text-2xl font-bold">
+          {`${minutes}:${formattedSeconds}`}
+        </div>
 
-      <div className=" flex items-end justify-center gap-2 ">
-        {!isRunning ? (
-          <button
-            className="flex justify-center items-center h-8 w-12 rounded-full px-2 p-1 bg-gradient-to-br from-main100/50 to-main200/50 hover:from-main100 hover:to-main200 duration-100 transition-all "
-            onClick={handleStart}
-            disabled={isRunning}
-          >
-            <BsFillPlayFill />
-          </button>
-        ) : (
-          <section className="rounded-full h-8  px-2 p-1  flex justify-between items-center">
+
+{!isRunning ? (
             <button
-              onClick={handlePause}
-              disabled={!isRunning}
-              className="rounded-l-full w-12 h-8 flex justify-center items-center bg-gradient-to-br from-main100/50 to-main200/50 hover:from-main100 hover:to-main200 duration-100 transition-all"
+               className="duration-100 transition-all hidden group-hover:flex z-30 absolute w-full  h-full  justify-center items-center text-2xl font-bold"              
+               onClick={handleStart}
+              disabled={isRunning}
             >
-              <TbPlayerPauseFilled />
+              <BsFillPlayFill />
             </button>
+          ) : (
+            <section className="gap-5 duration-100 transition-all hidden group-hover:flex z-30 absolute w-full  h-full  justify-center items-center text-2xl font-bold">
+              <button
+                onClick={handlePause}
+                disabled={!isRunning}
+                className="rounded-l-full w-12 h-8 flex justify-center items-center  duration-100 transition-all"
+              >
+                <TbPlayerPauseFilled />
+              </button>
+              <button
+                className="rounded-r-full w-12 h-8 flex justify-center items-center  duration-100 transition-all"
+                onClick={handleReset}
+              >
+                <MdOutlineReplayCircleFilled />
+              </button>
+            </section>
+          )}
+
+
+
+
+
+      </section>
+      {/* <section className="fixed bottom-0 right-1/2 left-1/2 -translate-x-[50%] rounded-t-full  bg-darkform p-2 gap-2 flex flex-col justify-center items-center w-32">
+        <div className="w-16">
+          <CircularProgressbar
+            className={`${
+              isRunning
+                ? "fill-white stroke-main100/80"
+                : "fill-white/50 stroke-main100/40"
+            }`}
+            value={(seconds * 100) / session}
+            text={`${minutes}:${formattedSeconds}`}
+          />
+        </div>
+
+        <div className=" flex items-end justify-center gap-2 ">
+          {!isRunning ? (
             <button
-              className="rounded-r-full w-12 h-8 flex justify-center items-center bg-gradient-to-br from-main100/50 to-main200/50 hover:from-main100 hover:to-main200 duration-100 transition-all"
-              onClick={handleReset}
+              className="flex justify-center items-center h-8 w-12 rounded-full px-2 p-1 bg-gradient-to-br from-main100/50 to-main200/50 hover:from-main100 hover:to-main200 duration-100 transition-all "
+              onClick={handleStart}
+              disabled={isRunning}
             >
-              <MdOutlineReplayCircleFilled />
+              <BsFillPlayFill />
             </button>
-          </section>
-        )}
-      </div>
-    </section>
+          ) : (
+            <section className="rounded-full h-8  px-2 p-1  flex justify-between items-center">
+              <button
+                onClick={handlePause}
+                disabled={!isRunning}
+                className="rounded-l-full w-12 h-8 flex justify-center items-center bg-gradient-to-br from-main100/50 to-main200/50 hover:from-main100 hover:to-main200 duration-100 transition-all"
+              >
+                <TbPlayerPauseFilled />
+              </button>
+              <button
+                className="rounded-r-full w-12 h-8 flex justify-center items-center bg-gradient-to-br from-main100/50 to-main200/50 hover:from-main100 hover:to-main200 duration-100 transition-all"
+                onClick={handleReset}
+              >
+                <MdOutlineReplayCircleFilled />
+              </button>
+            </section>
+          )}
+        </div>
+      </section> */}
+    </>
   );
 }
 
