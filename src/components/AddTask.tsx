321d41fc553967097d0f3753a39971b5ec7917b3
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-03-19 22:49:54 +0100

    added counter

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -7,7 +7,7 @@ const AddTask = () => {
     <>
       <Dialog>
         <DialogTrigger className="w-full flex justify-center items-center">
-          <section className="max-w-xl text-font/50 hover:text-main200 gap-2  cursor-pointer select-none h-20 p-2 w-full rounded-xl border-2 border-font/50 border-dashed hover:border-main200 flex justify-center items-center flex-col active:text-main100 active:border-main100 duration-100 transition-all">
+          <section className="max-w-xl text-font/50 hover:text-main200 gap-2  cursor-pointer select-none h-20 p-2 w-full rounded-xl border-2 border-font/50 border-dashed hover:border-main200 flex justify-center items-center flex-col active:brightness-110 duration-100 transition-all">
             <MdOutlineAdd className="h-8 w-8" />
             <h1 className="font-bold text-xl">Add New Task</h1>
           </section>
--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -1,12 +1,28 @@
+"use client"
+import { useState } from "react";
+import Counter from "./Counter";
 
 const AddTaskForm = () => {
+    const [count, setCount] = useState<number>(1);
   return (
-    <form className="w-full p-2 flex justify-center items-center flex-wrap">
-      <input
-        placeholder="write your task name"
-        type="text"
-        className="placeholder:text-font/20 focus:placeholder:text-font/50 w-full h-10 p-1 px-2 rounded-lg bg-black/40 border-2 outline-none border-main100/20 focus:border-main100"
-      />
+    <form className="w-full p-2 py-5 gap-2 flex justify-center  flex-col">
+      <section className="flex flex-col ">
+        <label className="px-3 text-sm font-normal text-font/80">
+          Task name :
+        </label>
+        <input
+          placeholder="write your task name"
+          type="text"
+          className="placeholder:text-font/20 focus:placeholder:text-font/50 w-full h-10 p-1 px-2 rounded-lg bg-black/40 border-2 outline-none border-main100/20 focus:border-main100"
+        />
+      </section>
+
+      <section className="px-2 flex flex-col ">
+        <label className="px-1 text-sm font-normal text-font/80">
+          Number of rounds :
+        </label>
+        <Counter count={count} setCount={setCount} max={20} />
+      </section>
     </form>
   );
 }
